6a438ec3776fe10f21702c2a664877823c22168f
Ghevond Tunyan <ghtunyan2018@gmail.com>
2022-07-05 13:04:43 +0200

    get familiar with strings as data type and its methods

--- strings.py
+++ strings.py
@@ -1,1 +1,12 @@
-print('asd')
+phrase = "Aregak Tours"
+
+print(phrase)
+
+# replace("Aregak","Sun")
+# index('g')
+# phrase[0]
+# len() length of the string
+# lower()
+# upper()
+# islower() returns boolean
+# isupper() returns boolean
